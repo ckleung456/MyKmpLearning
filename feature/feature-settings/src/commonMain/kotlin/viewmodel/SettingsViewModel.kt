c915db2bd754f71fe6b677c8d4f28072a44b7e4c
ck <liangyu456@gmail.com>
2026-09-18 23:41:25 -0700

    quick demo of how to access data from another feature in current project setup

--- feature/feature-settings/src/commonMain/kotlin/viewmodel/SettingsViewModel.kt
+++ feature/feature-settings/src/commonMain/kotlin/viewmodel/SettingsViewModel.kt
@@ -1,26 +1,51 @@
 package viewmodel
 
+import CountryFeatureApi
 import FeatureEventBus
+import FeatureRegistry
 import OpenCountriesEvent
 import UiState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import model.domain.MockSettingsData
+import model.domain.NavigationSettingItem
 import model.domain.ToggleSettingItem
 import model.ui.setting.SettingsAction
 import model.ui.setting.SettingsState
 
 class SettingsViewModel(
+    private val featureRegistry: FeatureRegistry,
     private val featureEventBus: FeatureEventBus
 ) : ViewModel() {
 
-    private val _state = MutableStateFlow<UiState<SettingsState>>(
-        UiState.Success(SettingsState(items = MockSettingsData.initial()))
-    )
-    val state = _state.asStateFlow()
+    private val countryFeatureVersion: String by lazy {
+        featureRegistry.getFeature<CountryFeatureApi>()?.version.orEmpty()
+    }
+
+    private val _state = MutableStateFlow<UiState<SettingsState>>(UiState.Loading)
+    val state = _state
+        .onStart {
+            MockSettingsData.initial().toMutableList().map { item ->
+                when (item.id) {
+                    MockSettingsData.OPEN_COUNTRIES_ID -> NavigationSettingItem(id = MockSettingsData.OPEN_COUNTRIES_ID, label = "Browse Countries, version: $countryFeatureVersion")
+                    else -> item
+                }
+            }.toImmutableList().let { newItems ->
+                _state.update {
+                    UiState.Success(SettingsState(newItems))
+                }
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = UiState.Loading
+        )
 
     fun onAction(action: SettingsAction) {
         when (action) {
